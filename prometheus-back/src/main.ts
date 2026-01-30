97182fd4e73e1da7817eb0c679226fd0f98adebb
Nikushaana <nikanashvili@gmail.com>
2026-01-31 02:37:11 +0400

    fix: error

--- prometheus-back/src/main.ts
+++ prometheus-back/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://prometheus-jobs.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://prometheus-jobs.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
   });
